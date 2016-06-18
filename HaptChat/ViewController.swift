d0339e47c794ad807fea9c91c063aa125188dfd6
shashi kumar <shashi@toppr.com>
2016-06-18 09:49:24 +0530

    Initial Commit

---
+++ HaptChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HaptChat
+//
+//  Created by shashi kumar on 18/06/16.
+//  Copyright © 2016 Shashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
